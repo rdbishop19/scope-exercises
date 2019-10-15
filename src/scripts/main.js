673e2497d45272dc84e437cc05308852ee20eaae
Ryan Bishop <rdbishop06@gmail.com>
2019-10-15 10:27:15 -0500

    Fix conjuction function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,17 @@
 console.log("Scope scope scope");
 
-const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
-let y = 1
+// const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+// let y = 1
 
-for (let y = 0; y < cookies.length; y++) {
-    const currentCookie = cookies[y]
-    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// for (let y = 0; y < cookies.length; y++) {
+//     const currentCookie = cookies[y]
+//     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// }
+
+const conjunction = function (firstWord, secondWord) {
+    // const conjoinedWord = `${firstWord} ${secondWord}` // local scope
+    return `${firstWord} ${secondWord}`
 }
+
+const conjoinedWord = conjunction("Master", "Card")  // global scope
+console.log(conjoinedWord)
