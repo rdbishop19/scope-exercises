d4f50c1683fd98c7cde3c1c2f96d685f90fb0b10
Ryan Bishop <rdbishop06@gmail.com>
2019-10-15 10:34:01 -0500

    Fix mod squad. Aggregate items to DOM and show

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,10 +8,27 @@ console.log("Scope scope scope");
 //     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
 // }
 
-const conjunction = function (firstWord, secondWord) {
-    // const conjoinedWord = `${firstWord} ${secondWord}` // local scope
-    return `${firstWord} ${secondWord}`
-}
+// const conjunction = function (firstWord, secondWord) {
+//     // const conjoinedWord = `${firstWord} ${secondWord}` // local scope
+//     return `${firstWord} ${secondWord}`
+// }
 
-const conjoinedWord = conjunction("Master", "Card")  // global scope
-console.log(conjoinedWord)
+// const conjoinedWord = conjunction("Master", "Card")  // global scope
+// console.log(conjoinedWord)
+
+{
+    const ModSquad = {
+        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+        "series": {
+            "start": "1968",
+            "end": "1973"
+        }
+    }
+
+    let HTMLRepresentation = `<h1>The Mod Squad</h1>`
+
+    ModSquad.members.forEach(member => {
+        HTMLRepresentation += `<div>${member}</div>`
+    })
+    document.querySelector(".show-info").innerHTML = HTMLRepresentation
+}
